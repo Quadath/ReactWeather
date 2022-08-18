b6ccf8c5fa13328cb15d4b55e9ed7110ec287942
Quadatz <quadath@gmail.com>
2022-08-18 17:26:04 +0300

    city input

---
+++ src/components/input/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const Input = () => (
+    <div className="input-wrap">
+        <input className="input-field"/>
+        <button className="input-button">+</button>
+    </div>
+)
