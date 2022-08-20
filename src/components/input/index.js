d55f3799d38f34b0885e6e0135709cf6b2eb9a40
Quadatz <quadath@gmail.com>
2022-08-20 10:42:22 +0300

    use state

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
 import './input.css'
 
 export const Input = () => {
-    const handleOnChange = () => {
-    }
     return (
-        <div className="input-wrap">
-            <input className="input-field" onChange={handleOnChange}/>
-            <button className="input-button">+</button>
-        </div>
+    <div className="input-wrap">
+        <InputTag/>
+        <Button/>
+    </div> 
     )
 }
+
+const InputTag = () => {
+    const [inputValue, setInputValue] = useState('');
+    const handleOnChange = (e) => {
+        setInputValue(e.target.value)
+    }
+    return (
+        <input className="input-field" placeholder="Enter city..." value={inputValue} onChange={handleOnChange} />
+    )
+}
+
+const Button = () => (<button className="input-button">+</button>)
+
